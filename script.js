1ca94ebb1b5ab03207dd8f5d15c129faec28df91
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-09-15 23:35:54 +0530

    code

---
+++ script.js
@@ -0,0 +1,17 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf) 
+
+
+function abcd(){
+    
+}
